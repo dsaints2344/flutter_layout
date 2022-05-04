531a47e406d5a6fc7e17790f00c9f215ff780241
David De Los Santos <ddelossantosmateo@gmail.com>
2022-05-03 20:57:29 -0400

    Creating stack example

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Rows and Columns'),
         ),
         backgroundColor: Colors.indigo[100],
-        body: Home(),
+        body: const Home(),
       ),
     );
   }
@@ -26,13 +26,11 @@ class Home extends StatelessWidget {
     final sizeX = MediaQuery.of(context).size.width;
     final sizeY = MediaQuery.of(context).size.height;
     return Container(
-      width: sizeX,
-      height: sizeY,
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: createSquares(5)),
-    );
+        width: sizeX,
+        height: sizeY,
+        child: Stack(
+          children: showPizzaLayout(sizeX, sizeY),
+        ));
   }
 
   List<Widget> createSquares(int numSquares) {
@@ -57,5 +55,65 @@ class Home extends StatelessWidget {
       squares.add(square);
     }
     return squares;
+  }
+
+  List<Widget> showPizzaLayout(double sizeX, double sizeY) {
+    List<Widget> layoutChildren = [];
+    Container background = Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage('http://bit.ly/pizza_image'),
+              fit: BoxFit.fitHeight)),
+    );
+
+    layoutChildren.add(background);
+    Positioned pizzaCard = Positioned(
+        top: sizeY / 20,
+        left: sizeX / 20,
+        child: Card(
+          elevation: 12,
+          color: Colors.white70,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          child: Column(
+            children: [
+              Text(
+                "Pizza 1",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange[800]),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(12),
+                child: Text(
+                  'This pizza is made of tomato, \n Mozzarella and Basil. \n\n You can\'t miss it',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              )
+            ],
+          ),
+        ));
+    layoutChildren.add(pizzaCard);
+    Positioned buttonOrder = Positioned(
+      bottom: sizeY / 20,
+      left: sizeX / 20,
+      width: sizeX - sizeY / 10,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            elevation: 12,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            primary: Colors.orange[900]),
+        child: const Text(
+          'Order Now!',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+    layoutChildren.add(buttonOrder);
+    return layoutChildren;
   }
 }
